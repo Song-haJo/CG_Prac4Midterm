b129fa733beb9755cc4e1285d1f8bd987e7041c3
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-04-25 11:28:12 +0900

    Update p5.js

--- 1. 펜 시뮬레이션 하시오./p5.js
+++ 1. 펜 시뮬레이션 하시오./p5.js
@@ -1,1 +1,11 @@
+function setup() {
+  createCanvas(600, 600);     //canvas size
+  stroke(0, 0, 255);          //stroke color
+  strokeWeight(8);            //stroke thickness
+}
 
+function draw() {
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
