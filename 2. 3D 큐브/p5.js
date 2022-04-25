7cc282646f23d0920509a0db10b62e0c769e95f3
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-04-25 11:31:32 +0900

    Update p5.js

--- 2. 3D 큐브/p5.js
+++ 2. 3D 큐브/p5.js
@@ -1,1 +1,11 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
 
+function draw() {
+  background(220);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(100);
+}
+// https://editor.p5js.org/Francesca.Fabian/sketches/Syfq0rE0Q
